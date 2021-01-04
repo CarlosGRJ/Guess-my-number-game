febc7ad33fd685b1060e6e0276df8c2cde7366ae
CarlosGRJ <carlosgrj@outlook.com>
2021-01-04 09:05:56 -0600

    Game Logic pt-2

--- starter/script.js
+++ starter/script.js
@@ -10,21 +10,27 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', () => {
     const guess = Number(document.querySelector('.guess').value);
 
+    // When there is no input
     if (!guess) {
 
         document.querySelector('.message').textContent = '⛔ No number!'
 
+        // When player wins
     } else if (guess === secretNumber) {
 
-        document.querySelector('.message').textContent = '🎉 Correct Number!'
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347'
 
+        document.querySelector('.number').style.width = '30rem';
+
+        // When guess is too high
     } else if (guess > secretNumber) {
 
         if (score > 1) {
@@ -36,7 +42,7 @@ document.querySelector('.check').addEventListener('click', () => {
             document.querySelector('.score').textContent = 0;
         }
 
-
+        // When guess is too low
     } else if (guess < secretNumber) {
 
         if (score > 1) {
@@ -49,4 +55,17 @@ document.querySelector('.check').addEventListener('click', () => {
         }
 
     }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
 });
